1a2b4226897375fd0ada9c051a10a9ff37be68ab
ArturBaur <artur.baur@gmail.com>
2023-11-10 00:12:27 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/goit-react-hw-01-components/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
